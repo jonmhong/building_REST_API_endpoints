6d053b544ead3a6fff45ae9570e54faaf7021dfd
Jonathan Hong <jonmhong@gmail.com>
2016-11-01 00:06:50 -0700

    Refactor: separated geocode locator
    Separated get_geocode_location() function and created a new file

---
+++ geocode_locator.py
@@ -0,0 +1,17 @@
+import httplib2
+import json
+
+
+google_api_key = "AIzaSyANbOmmApPNwoUOJ_u1q4MnohifOsMAO9A"
+
+def get_geocode_location(inputString):
+	global google_api_key
+	locationString = inputString.replace(" ", "+")
+	url = ("https://maps.googleapis.com/maps/api/geocode/json?address=%s&key=%s" % (locationString, google_api_key))
+	h = httplib2.Http()
+	response, content = h.request(url, 'GET')
+	result_data = json.loads(content)
+	lat = result_data['results'][0]['geometry']['location']['lat']
+	lng = result_data['results'][0]['geometry']['location']['lat']
+
+	return (lat, lng)
