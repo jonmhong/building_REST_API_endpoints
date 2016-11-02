952f8ff75a88bc40e4dd31f61ef8a8e31efaf26d
Jonathan Hong <jonmhong@gmail.com>
2016-11-02 01:13:20 -0700

    Style, Test
    Style: cleared some debugging notes
    
    Test: added more tests to check if the endpoint sends a proper status and response for GET, PUT, and DELETE method requests

--- tester.py
+++ tester.py
@@ -14,14 +14,13 @@ address = raw_input("Enter the address of the server you want to access,\nIf lef
 if address == '':
 	address = 'http://localhost:5000'
 
-# First Test (of 5)
+# POST Test
 try:
 	print "Test 1: Creating new Restaurants..."
 	url = address + '/restaurants?location=Buenos+Aires+Argentina&mealType=Sushi'
 	h = httplib2.Http()
 	response, result = h.request(url, 'POST')
 
-	# refer to: https://www.w3.org/Protocols/rfc2616/rfc2616-sec10.html
 	if response['status'] != '200': # the satus code class of 2xx means successful
 		raise Exception("Received an unsuccessful status code of %s" % response['status'])
 	print json.loads(result)
@@ -62,10 +61,82 @@ except Exception as err:
 	print "Test 1 FAILED: Could not add new restaurants"
 	print err.args
 	sys.exit()
-
+	
 else:
 	print "Test 1 PASS: Successfully Made all new restaurants"
 
 
-# Second Test
+# GET Test
+try:
+	print "Attempting Test 2: Reading all Restaurants: "
+	url = address + "/restaurants"
+	h = httplib2.Http()
+	response, result = h.request(url, 'GET')
+	if response['status'] != '200':
+		raise Exception('Received an unsuccessful status code of %s' % response['status'])
+	json_result = json.loads(result)
+	print json_result
 
+except Exception as err:
+	print "Test 2 FAILED: Could not find any restaurants"
+	print err.args
+
+else:
+	print "Test 2 PASS: Successfully made all new restaurants"
+
+
+# Specific Restaurant GET Test
+try:
+	print "Attempting Test 3: Reading a specific restaurant"
+	all_restaurants = json_result
+	rest_id = all_restaurants['restaurants'][len(all_restaurants['restaurants']) - 1]['id']
+	restaurant_url = address + "/restaurants/%s" % rest_id
+	h = httplib2.Http()
+	response, result = h.request(restaurant_url, 'GET')
+	if response['status'] != '200':
+		raise Exception('Received an unsuccessful status code of %s' % response['status'])
+	print json.loads(result)
+
+except Exception as err:
+	print "Test 3 FAILED: Could not find specific restaurant with id %s " % rest_id
+	print err.args
+else:
+	print "Test 3 PASS: Successfully made GET request"
+
+
+# PUT Test
+try:
+	print "Changing the name, image, and address of the first restaurant in the database"
+	h = httplib2.Http()
+	rest_id = json_result['restaurants'][0]['id']
+	url = address + "/restaurants/%s?name=Udacity&address=2465+Latham+Street+Mountain+View+CA&image=https://media.glassdoor.com/l/70/82/fc/e8/students-first.jpg" % rest_id
+
+	response, result = h.request(url, 'PUT')
+
+	if response['status'] != '200':
+		raise Exception("Received an unsuccessful status code of %s" % response['status'])
+	print json.loads(result)
+
+except Exception as err:
+	print "Test 4 FAILED: Couldn't update restaurant from server"
+	print err.args
+else:
+	print "Test 4 PASS: Successfully updated first restaurant"
+
+
+# DELETE Test
+try:
+	print "Attempting Test 5: Deleting the second restaurant from the server"
+	rest_id = json_result['restaurants'][1]['id']
+	url = address + "/restaurants/%s" % rest_id
+	h = httplib2.Http()
+	response, result = h.request(url, 'DELETE')
+	if response['status'] != '200':
+		raise Exception("Received an unsuccessful status code of %s" % response['status'])
+	print result
+
+except Exception as err:
+	print "Test 5 FAILED: Could not find and delete restaurant from server"
+	print err.args
+else:
+	print "Test 5 PASS: Successfully deleted restaurant from server"
