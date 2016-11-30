1422434e012cc14b7aa1202d01094f4ef22e6e78
Jonathan Hong <jonmhong@gmail.com>
2016-11-30 13:17:32 -0800

    Fix: Now able to receive token
    Fixed bug that prevented server from sending a token. Set g.user

--- endpoint.py
+++ endpoint.py
@@ -1,47 +1,74 @@
-from models import Base, User, LinkedIn
+from user_models import Base, User, LinkedIn
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, relationship
 from sqlalchemy import create_engine
 from flask import Flask, jsonify, request, url_for, abort, g
 from flask_httpauth import HTTPBasicAuth
+import itsdangerous
+import json
 
 auth = HTTPBasicAuth()
 
+engine = create_engine('sqlite:///user_info.db', echo=True) # returns an instance of Engine
 
-engine = create_engine('sqlite:///users.db')
 Base.metadata.bind = engine
+
 DBSession = sessionmaker(bind=engine)
+
 session = DBSession()
+
 app = Flask(__name__)
 
 
 @auth.verify_password
-def verify_password(username, password):
-	user = session.query(User).filter_by(username=username).first()
-	if not user or not user.verify_password(password):
-		print "Incorrect user or password"
-		return False
+def verify_password(username_or_token, password):
+	#user = session.query(User).filter_by(username=username).first()
+	# if not user or not user.verify_password(password):
+	# 	print "Incorrect user or password"
+	# 	return False
+	# else:
+	# 	g.user = user
+	# 	return True
+	user_id = User.verify_auth_token(username_or_token)
+	if user_id:
+		user = session.query(User).filter_by(id=user_id).first()
 	else:
-		g.user = user
-		return True
+		user = session.query(User).filter_by(username=username_or_token).first()
+		if not user or not user.verify_password(password):
+			return False
+
+	g.user = user
+	return True
 
 
+@app.route('/token')
+@auth.login_required
+def get_auth_token():
+	# generate special auth token for user
+	name = request.json.get('username')
+	user = session.query(User).filter_by(username=name).first()
+	g.user = user
+	token = g.user.generate_auth_token()
+	return jsonify({'token': token}) #token.decode('ascii')
+
+###
 @app.route('/users', methods=['POST'])
 def add_user():
-	user = request.args.get('username', '')
-	pw = request.args.get('password', '')
+	user = request.json.get('username')
+	pw = request.json.get('password')
 
 	if user is None or pw is None:
 		raise Exception("Please enter both a username and password")
 
 	if session.query(User).filter_by(username=user).first() is not None:
-		raise Exception("Username already exists")
+		user = session.query(User).filter_by(username=user).first()
+		return jsonify({'message': 'user already exists'}), 200
 
 	new_user = User(username=user)
 	new_user.hash_password(pw)
 	session.add(new_user)
 	session.commit()
-	return jsonify({'username': new_user.username})
+	return jsonify({'username': new_user.username}), 201
 
 
 @app.route('/users/<int:id>')
@@ -50,6 +77,13 @@ def get_user(id):
 	if not user:
 		abort(400)
 	return jsonify({'username': user.username})
+
+@app.route('/testresource', methods=['GET'])
+@auth.login_required
+def test_access():
+	user = request.json.get('username')
+	user = session.query(User).filter_by(username=user).first()
+	return jsonify({'username': user})
 
 
 @app.route('/resource')
@@ -72,7 +106,7 @@ def show_linkedin():
 		new_linkedin = LinkedIn(name=get_name, link=get_link, description=get_description)
 		session.add(new_linkedin)
 		session.commit()
-		return jsonify(new_linkedin.serialize)
+		return jsonify(new_linkedin.serialize), 201
 
 
 if __name__ == '__main__':
